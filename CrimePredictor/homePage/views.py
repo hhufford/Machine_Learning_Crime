3a159087c50bc5c3e52b7b64e5537df5714a6a43
heidihufford <heidihufford@mymail.mines.edu>
2019-11-08 16:46:46 -0700

    Added initial page with title and button

--- CrimePredictor/homePage/views.py
+++ CrimePredictor/homePage/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 from django.template import loader
 
 def index(request):
-    template = loader.get_template('test.html')
+    template = loader.get_template('index.html')
     context = {}
     return HttpResponse(template.render(context, request))

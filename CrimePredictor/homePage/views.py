17cbc341aa68818a79f7297128b0e1196c138a8d
hannahlevy <hlevy@mymail.mines.edu>
2019-11-08 17:41:31 -0700

    Added form input validation

--- CrimePredictor/homePage/views.py
+++ CrimePredictor/homePage/views.py
@@ -3,11 +3,20 @@ from django.http import HttpResponse
 from django.template import loader
 
 def index(request):
-    template = loader.get_template('index.html')
-    context = {}
-    return HttpResponse(template.render(context, request))
-	
-def submit(request):
-	template = loader.get_template('submit.html')
-	context = {}
-	return HttpResponse(template.render(context, request))
+    try:
+        in1 = float(request.POST["input1"])
+        in2 = float(request.POST["input2"])
+        in3 = float(request.POST["input3"])
+        in4 = float(request.POST["input4"])
+        in5 = float(request.POST["input5"])
+        result = machine_learning(in1,in2,in3,in4,in5)
+        template = loader.get_template('submit.html')
+        context = {'result' : result}
+        return HttpResponse(template.render(context, request))
+    except:
+        template = loader.get_template('index.html')
+        context = {}
+        return HttpResponse(template.render(context, request))
+
+def machine_learning(in1,in2,in3,in4,in5):
+    return True

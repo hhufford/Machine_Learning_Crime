963f390f34797890906a318e3fb2b4a72c9ca74a
heidihufford <heidihufford@mymail.mines.edu>
2019-12-01 12:14:35 -0700

    Added models

--- CrimePredictor/homePage/views.py
+++ CrimePredictor/homePage/views.py
@@ -2,14 +2,39 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+from source import ml_model
+
+import numpy as np
+#from tensorflow.keras.models import load_model
+#from tensorflow.keras.backend import clear_session
+#import joblib
+#import cv2
+import traceback
+
+crime_model = None
+
+def load_crime_model():
+    global crime_model
+    #model_file = 'models/keras-mnist.h5'
+    #mnist_model = load_model(model_file)
+    #mnist_model._make_predict_function()
+
+#Uses the Machine Learning model to predict the violent crime rate
+#Returns "Low", "Average", or "High"
+def machine_learning(in1):
+
+    return "Low"
+
 def index(request): 
+	load_crime_model
+
     try:
         in1 = float(request.POST["input1"])
-        in2 = float(request.POST["input2"])
-        in3 = float(request.POST["input3"])
-        in4 = float(request.POST["input4"])
-        in5 = float(request.POST["input5"])
-        result = machine_learning(in1,in2,in3,in4,in5)
+        #in2 = float(request.POST["input2"])
+        #in3 = float(request.POST["input3"])
+        #in4 = float(request.POST["input4"])
+        #in5 = float(request.POST["input5"])
+        result = machine_learning(in1)
         template = loader.get_template('index.html')
         context = {'result' : result}
         return HttpResponse(template.render(context, request))
@@ -18,7 +43,7 @@ def index(request):
         context = {}
         return HttpResponse(template.render(context, request))
 
-def machine_learning(in1,in2,in3,in4,in5):
-    return "Low"
+
+
 
 
